a91dc51954e6f8d87b7aaf487b8b081220cef6c8
AngelMValentin <angelvalentinusmc@gmail.com>
2025-04-26 13:55:06 -0700

    adding CRUD operations to notes

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('list/', views.list, name='notes_list'),
     path("create/", views.NoteCreate.as_view(), name="note_create"),
+    path("details/<int:pk>/", views.NoteDetail.as_view(), name='note_detail'),
+    path("update/<int:pk>/", views.NoteUpdate.as_view(), name='note_update'),
+    path("delete/<int:pk>/", views.NoteDelete.as_view(), name='note_delete'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .models import Note
 from django.urls import reverse_lazy
 from .forms import NoteForm
@@ -32,3 +32,31 @@ class NoteCreate(CreateView):
     form_class = NoteForm
     template_name = 'notes/create.html'
     success_url = reverse_lazy('notes_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form_title"] = "Create New Note"
+        return context
+
+class NoteDetail(DetailView):
+    template_name = 'notes/details.html'
+    model = Note
+
+class NoteUpdate(UpdateView):
+    template_name = 'notes/update.html'
+    model = Note
+    form_class = NoteForm
+    success_url = reverse_lazy("notes_list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form_title"] = "Update Note"
+        return context
+    
+
+class NoteDelete(DeleteView):
+    model = Note
+    template_name = 'note/delete.html'
+    success_url = reverse_lazy('notes_list')
+
+    
